042d5324a4e4ad45a97be74b5ba77793a87d7a5b
IvLaptev <ivlaptev13@ya.ru>
2022-07-21 07:04:14 +0300

    Fixed content-type error

--- gui/lib/resources/api_provider.dart
+++ gui/lib/resources/api_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
 import 'package:gui/models/motion.dart';
 
@@ -6,8 +8,11 @@ class ApiProvider {
 
   static Future sendMotions(List<Motion> motions) async {
     try {
-      await http.post(Uri.parse('$baseUrl/server/v1/motions'),
-          body: motionsToJson(motions));
+      await http.post(
+        Uri.parse('$baseUrl/server/v1/motions'),
+        body: motionsToJson(motions),
+        headers: {'Content-Type': ContentType.json.mimeType},
+      );
     } catch (e) {
       throw 'Can\'t send data to $baseUrl/server/v1/motions';
     }
