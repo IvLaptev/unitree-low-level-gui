3a59487f32284eec00f7d78264d79fd65bfda156
IvLaptev <ivlaptev13@ya.ru>
2022-11-12 01:07:51 +0300

    Fix api url

--- gui/lib/bloc/settings/settings_bloc.dart
+++ gui/lib/bloc/settings/settings_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gui/resources/api_provider.dart';
 
 part 'settings_events.dart';
 part 'settings_state.dart';
@@ -14,10 +15,12 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
   void _onHostChanged(HostChanged event, Emitter emit) {
     emit(SettingsState(
         host: event.host, port: state.port, secret: state.secret));
+    ApiProvider.baseUrl = 'http://${event.host}:${state.port}';
   }
 
   void _onPortChanged(PortChanged event, Emitter emit) {
     emit(SettingsState(
         host: state.host, port: event.port, secret: state.secret));
+    ApiProvider.baseUrl = 'http://${state.host}:${event.port}';
   }
 }
